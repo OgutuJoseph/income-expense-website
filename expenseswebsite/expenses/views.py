03fdc33452a590d25723cf675f1a6ceb4aed1ceb
OgutuJoseph <onyango.joseph@yahoo.com>
2024-08-06 20:05:22 +0300

    User Login, Logout and Access Control

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Expenses views.
 
+@login_required(login_url='authentication/login')
 def index(request):
     return render(request, 'expenses/index.html')
 
+@login_required(login_url='authentication/login')
 def add_expense(request):
     return render(request, 'expenses/add_expense.html')
